e49be85b1ac04b2e8422cda471389a045b780195
TheBugMaker <khalildev@duck.com>
2022-11-28 10:45:11 +0100

    First admin commit

---
+++ .admin/Static/js/admin.js
@@ -0,0 +1,30 @@
+const name = document.getElementById("username");
+const password = document.getElementById("password");
+let forgetPassword = document.getElementById("passRec");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+forgetPassword.addEventListener("click", () => {
+  alert("THIS LINK IS NOT ENABLED YET!");
+});
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (name.value === "username") {
+    messages.push("Username cannot be `username`");
+  }
+
+  if (password.value.length <= 6) {
+    messages.push("Password must be longer than 6 characters");
+  }
+  if (password.value.length >= 18) {
+    messages.push("Password must be less than 18 characters");
+  }
+  if (password.value === "password") {
+    messages.push("Password cannot be `password`");
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(`,\n`);
+  }
+});
